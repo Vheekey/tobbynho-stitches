e5bd79ae06bef70b43c22c2d11552848138de40a
Vheekey <vicformidable@gmail.com>
2020-04-16 18:53:36 +0100

    all authentications

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,7 @@ Route::prefix('/vendor')->name('vendor.')->namespace('Vendor')->middleware('vend
     
     //Admin View (Register and Login
     Route::get('/admin/sign', function(){
-        return view('auth\admin');
+        return view('auth.admin');
     });
 
     //admin register
